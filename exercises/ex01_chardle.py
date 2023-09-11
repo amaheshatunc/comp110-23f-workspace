7efe5f343ed352fd2cfb2b9dbf99a0a3a57b25d6
Arjun Mahesh <amahesh@ad.unc.edu>
2023-09-10 23:26:09 -0400

    First inputs inserted Exercise 01!

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,6 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+__author__ = "730668496"
+character_input: str = input("Enter a 5-character word: ")
+single_input: str = input("Enter a single character: ")
+
+print("Searching for " + single_input + " in " + character_input)
