1274ef10c15839cb200162b5210ac6448cf942cb
Arjun Mahesh <amahesh@ad.unc.edu>
2023-10-11 19:54:28 -0400

    Finished Ex04!

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,26 +1,29 @@
 __author__ = "730668496"
 
 def all(numbers: int, find: int) -> bool:
-    rewind = 0    
-    while rewind < len(numbers):
-        # check if the find is equal to any number in the list
-        if numbers[rewind] == find:
-            return True
-        rewind += 1
-    return False
-
-def max(input: list[int]) -> int:
-    if len(input) == 0:
-        raise ValueError("max() arg is an empty List")
     rewind = 0
-    highest = input[0]
+    if len(numbers) == 0:
+        return False
+        
+    while (rewind < len(numbers)):
+        # check if the find is equal to any number in the list
+        if numbers[rewind] != find:
+            return False
+        rewind += 1
+    return True
 
-    if len(input) == 1:
-        return input[0]
+def max(input_list: list[int]) -> int:
+    if len(input_list) == 0:
+        raise ValueError("max() arg is an empty List")
     
-    while rewind < len(input):
-        if input[rewind] > highest:
-            highest = input[rewind]
+    if len(input_list) == 1:
+        return input_list[0]
+    
+    rewind: int = 0
+    highest: int = input_list[0]    
+    while (rewind < len(input_list)):
+        if highest < input_list[rewind]:
+            highest = input_list[rewind]
         rewind += 1
     return highest
 
@@ -30,7 +33,6 @@ def is_equal(list_one: list[int], list_two: list[int]) -> bool:
         return False
     else:
         rewind: int = 0
-        
         while rewind < len(list_one):
             if list_one[rewind] != list_two[rewind]:
                 return False
