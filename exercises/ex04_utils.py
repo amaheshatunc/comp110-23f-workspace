ef2a2080848b7d7a563f5a56b746a04c71cb7863
Arjun Mahesh <amahesh@ad.unc.edu>
2023-10-11 20:07:42 -0400

    Finished EX04!!

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,16 +1,21 @@
+"""EX04 - list utility functions (fun!)!"""
 __author__ = "730668496"
 
-def all(numbers: int, find: int) -> bool:
-    rewind = 0
+
+def all(numbers: list[int], find: int) -> bool:
+    """This will check if the number is within the list or not."""
+    rewind: int = 0
     if len(numbers) == 0:
         return False
         
     while (rewind < len(numbers)):
-        # check if the find is equal to any number in the list
+        """"Check if the find is equal to any number in the list."""
         if numbers[rewind] != find:
             return False
+        """This checks if the find is equal to any number in the list."""
         rewind += 1
     return True
+
 
 def max(input_list: list[int]) -> int:
     if len(input_list) == 0:
@@ -22,13 +27,14 @@ def max(input_list: list[int]) -> int:
     rewind: int = 0
     highest: int = input_list[0]    
     while (rewind < len(input_list)):
+        """This function will return the highest number in a list."""
         if highest < input_list[rewind]:
             highest = input_list[rewind]
         rewind += 1
     return highest
 
+
 def is_equal(list_one: list[int], list_two: list[int]) -> bool:
-    
     if len(list_one) != len(list_two):
         return False
     else:
